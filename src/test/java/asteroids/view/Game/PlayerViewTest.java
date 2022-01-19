d5b9df499aaa8c4795539aa17fb460af871ca5c1
joao-teixeira02 <joao.teixeira2924@gmail.com>
2022-01-19 20:14:11 +0000

    Tests for new PlayerView

--- src/test/java/asteroids/view/Game/PlayerViewTest.java
+++ src/test/java/asteroids/view/Game/PlayerViewTest.java
@@ -2,42 +2,39 @@ package asteroids.view.Game;
 
 import asteroids.model.Entities.Player;
 import asteroids.model.Position;
-import com.googlecode.lanterna.TerminalPosition;
-import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class PlayerViewTest {
 
+    Player playerMock;
+    Position positionMock;
+    PlayerView playerView;
+    TextGraphics graphicsMock;
+
+    @BeforeEach
+    void init() {
+        playerMock = Mockito.mock(Player.class);
+        positionMock = Mockito.mock(Position.class);
+        Mockito.when(positionMock.getX()).thenReturn(10.0);
+        Mockito.when(positionMock.getY()).thenReturn(15.0);
+        Mockito.when(playerMock.getPosition()).thenReturn(positionMock);
+        playerView = Mockito.spy(new PlayerView(playerMock));
+        graphicsMock = Mockito.mock(TextGraphics.class);
+        playerView.setGraphics(graphicsMock);
+    }
+
     @Test
     void draw(){
         //given
-        Player playerMock = Mockito.mock(Player.class);
-        Mockito.when(playerMock.getPosition()).thenReturn(new Position(13.2,42.0));
-        Mockito.when(playerMock.getAngle()).thenReturn(0.123456789);
-        Mockito.when(playerMock.getRaio()).thenReturn(5.69);
-        PlayerView playerView = Mockito.spy(new PlayerView(playerMock));
-        TextGraphics graphicsMock = Mockito.mock(TextGraphics.class);
-        playerView.setGraphics(graphicsMock);
-
-        double anglePontaNave = playerMock.getAngle();
-        double anglePontaEsq = anglePontaNave + Math.PI*0.8333;   // 5/6
-        double anglePontaDir = anglePontaNave + Math.PI*1.1666;   // 7/6
+        Mockito.doNothing().when(playerView).drawImage(Mockito.any(), Mockito.anyInt(), Mockito.anyInt());
 
         //when
         playerView.draw();
 
         //then
-        Mockito.verify(playerView,Mockito.times(2)).getGraphics();
-        Mockito.verify(graphicsMock,Mockito.times(1)).setBackgroundColor(TextColor.Factory.fromString("#FFFFFF"));
-        Mockito.verify(graphicsMock,Mockito.times(1)).drawTriangle(
-                new TerminalPosition((int) (Math.cos(anglePontaNave)* playerMock.getRaio() + playerMock.getPosition().getX()),
-                        (int) (Math.sin(anglePontaNave)*playerMock.getRaio() + playerMock.getPosition().getY())),
-                new TerminalPosition((int) (Math.cos(anglePontaEsq)* playerMock.getRaio() + playerMock.getPosition().getX()),
-                        (int) (Math.sin(anglePontaEsq)*playerMock.getRaio() + playerMock.getPosition().getY())),
-                new TerminalPosition((int) (Math.cos(anglePontaDir)* playerMock.getRaio() + playerMock.getPosition().getX()),
-                        (int) (Math.sin(anglePontaDir)*playerMock.getRaio() + playerMock.getPosition().getY())),
-                ' ');
+        Mockito.verify(playerView, Mockito.times(1)).drawImage(PlayerView.playerDraw, 10, 15);
     }
 }
