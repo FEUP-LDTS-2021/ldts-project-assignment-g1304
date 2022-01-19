8349e3edb19fc6239a3fca7ce7b69885febcfa42
joao-teixeira02 <joao.teixeira2924@gmail.com>
2022-01-19 00:41:19 +0000

    Music: Implementation

--- src/main/java/asteroids/control/Music.java
+++ src/main/java/asteroids/control/Music.java
@@ -1,20 +1,56 @@
 package asteroids.control;
 
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.sound.sampled.FloatControl;
+import java.io.File;
 
-public interface Music {
+public class Music {
 
-    boolean isPlaying();
+    private Clip sound;
 
-    Clip getSound();
+    public Music(String sound) {
+        this.sound = loadSound(sound);
+    }
 
-    void setSound(Clip sound);
+    public boolean isPlaying(){return sound.isRunning();}
 
-    Clip loadSound(String sound);
+    public Clip getSound() {
+        return sound;
+    }
 
-    void startLoop();
+    public void setSound(Clip sound) {
+        this.sound = sound;
+    }
 
-    void start();
 
-    void stop();
+    private Clip loadSound(String sound) throws NullPointerException{
+        try {
+            String rootPath = new File(System.getProperty("user.dir")).getPath();
+            File musicFile = new File(rootPath+sound);
+            AudioInputStream audioInput = AudioSystem.getAudioInputStream(musicFile);
+            Clip musicClip = AudioSystem.getClip();
+            musicClip.open(audioInput);
+            FloatControl gainControl = (FloatControl) musicClip.getControl(FloatControl.Type.MASTER_GAIN);
+            gainControl.setValue(-25.0f);
+            return musicClip;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public void startLoop() {
+        sound.setMicrosecondPosition(0);
+        sound.start();
+        sound.loop(Clip.LOOP_CONTINUOUSLY);
+    }
+
+    public void start() {
+        sound.setMicrosecondPosition(0);
+        sound.start();
+    }
+
+    public void stop() {sound.stop();}
 }
--- src/main/java/asteroids/control/MusicManager.java
+++ src/main/java/asteroids/control/MusicManager.java
@@ -1,24 +1,82 @@
 package asteroids.control;
 
-public interface MusicManager {
+public class MusicManager {
 
-    MusicManager getInstance();
+    private static Music soundTrack;
+    private static Music shoot;
+    private static Music rocket;
+    private static Music destruction;
+    private static Music gameOver;
+    private static MusicManager musicManager;
 
-    void setSoundTrack(Music soundTrack);
+    private MusicManager() {
+        this.soundTrack = new Music("/src/main/resources/Sounds/soundTrack.wav");
+        this.shoot = new Music("/src/main/resources/Sounds/shoot.wav");
+        this.rocket = new Music("/src/main/resources/Sounds/rocket.wav");
+        this.destruction = new Music("/src/main/resources/Sounds/destruction.wav");
+        this.gameOver = new Music("/src/main/resources/Sounds/gameOver.wav");
+    }
 
-    void setShoot(Music shoot);
+    public static MusicManager getInstance() {
+        if (musicManager == null) {
+            musicManager = new MusicManager();
+        }
+        return musicManager;
+    }
 
-    void setRocket(Music rocket);
+    public static void setSoundTrack(Music soundTrack) {
+        MusicManager.soundTrack = soundTrack;
+    }
 
-    void setDestruction(Music destruction);
+    public static void setShoot(Music shoot) {
+        MusicManager.shoot = shoot;
+    }
 
-    void setGameOver(Music gameOver);
+    public static void setRocket(Music rocket) {
+        MusicManager.rocket = rocket;
+    }
 
-    void start(Sounds sound);
+    public static void setDestruction(Music destruction) {
+        MusicManager.destruction = destruction;
+    }
 
-    void stop(Sounds sound);
+    public static void setGameOver(Music gameOver) {
+        MusicManager.gameOver = gameOver;
+    }
 
-    boolean isPlaying(Sounds sound);
+    public void start(Sounds sound) {
+        switch(sound) {
+            case SOUNDTRACK -> soundTrack.startLoop();
+            case SHOOT -> shoot.start();
+            case ROCKET -> rocket.startLoop();
+            case DESTRUCTION -> destruction.start();
+            case GAMEOVER -> gameOver.start();
+        }
+    }
 
-    void stopAll();
+    public void stop(Sounds sound) {
+        switch(sound) {
+            case SOUNDTRACK -> soundTrack.stop();
+            case ROCKET -> rocket.stop();
+            case GAMEOVER -> gameOver.stop();
+        }
+    }
+
+    public boolean isPlaying(Sounds sound) {
+        return switch (sound) {
+            case SOUNDTRACK -> soundTrack.isPlaying();
+            case SHOOT -> shoot.isPlaying();
+            case ROCKET -> rocket.isPlaying();
+            case DESTRUCTION -> destruction.isPlaying();
+            case GAMEOVER -> gameOver.isPlaying();
+        };
+    }
+
+    public void stopAll() {
+        soundTrack.stop();
+        shoot.stop();
+        rocket.stop();
+        destruction.stop();
+        gameOver.stop();
+    }
 }
